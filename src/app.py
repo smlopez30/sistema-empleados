a748e3a39159be33458301f43512da0b48d061f5
smlopez30 <smlopez@gmail.com>
2023-06-29 21:29:57 -0300

    instancia flask

--- src/app.py
+++ src/app.py
@@ -1,2 +1,10 @@
 from flask import Flask
-print('Hola Mundo')
+from flask import render_template
+from flaskext.myl import MySQL
+
+app = Flask(__name__)
+mysql = MySQL
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
