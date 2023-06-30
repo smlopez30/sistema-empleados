915c24f0d4a70c1b5df982e0e11b9d181db2a055
smlopez30 <smlopez@gmail.com>
2023-06-29 23:22:29 -0300

    Conexion a mysql y test

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
-from flask import render_template
-from flaskext.myl import MySQL
+from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-mysql = MySQL
+mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'semilo33'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO empleados (nombre, correo, foto) VALUES ('Juan', 'juan@email.com','fotodejuan.jpg');"
+    cursor.execute(sql)
+    conn.commit()
+
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
